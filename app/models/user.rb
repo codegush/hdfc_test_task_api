237f4346ad416ccada297e3e00286d7c6f331b0b
Hemant <hemantmanwani.iitr@gmail.com>
2018-04-24 18:37:45 +0530

    Setup project on rails 5.1 1. Crate rails 5.1 project for backend api on ruby 2.5.1 2. Setup routes, controllers for user and sesssions. 3. Setup basic user module

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  has_secure_password
+  validates :email, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
+  validates :password, confirmation: true 
+  validates :password_confirmation, presence: true
+end
